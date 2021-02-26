30a38572dc4d3e164bd19e6f6bffe31706760ad1
Roman Zemeła <32485779+romanzemela@users.noreply.github.com>
2021-02-26 20:59:31 +0100

    tlumaczenie

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -13,151 +13,133 @@ import imutils
 import time
 import cv2
 import os
+import RPi.GPIO as GPIO #piny do diod led
 from plyer import notification
 
-def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+def wykryj_i_oszacuj_maske(klatka, faceNet, maskNet):
+	# Wyciągnięcie rozmiarów klatki i storzenie z nich obiektu BLOB
+	# Obiekty BLOB sluza do przechowywania duzych obiektow multimedialnych, np. obrazow
+	(h, w) = klatka.shape[:2]
+	blob = cv2.dnn.blobFromImage(klatka, 1.0, (300, 300),
+		(104.0, 177.0, 123.0)) #
 
-	# pass the blob through the network and obtain the face detections
+	# Uzycie sieci "faceNet" by wykryc twarze znajdujace sie na ujeciu z kamery
 	faceNet.setInput(blob)
-	detections = faceNet.forward()
+	wykryte_twarze = faceNet.forward()
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
-	faces = []
+	# inicjalizacja tablicy twarzy, ich lokacji i prawdopodobienst na maski
+	twarze = []
 	locs = []
-	preds = []
+	prawdopodobienstwo_maski = []
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
+	# iterowanie po wykrytych twarzach
+	for i in range(0, wykryte_twarze.shape[2]):
+		# pobranie wartosci prawdopodoienstw. Prawdopodobienstwo mowi nam jaka jest szansa, ze
+		# algorytm sie nie pomylil
+		prawdopodobienstwo = wykryte_twarze[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		# Usuniece twarzy ktore maja prawdopodobienstwo mniejsze od min
+		if prawdopodobienstwo > args["prawdopodobienstwo"]:
+			# obliczenie koordynatow twarzy na ujeciu
+			box = wykryte_twarze[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
+			# upewnienie sie, ze obliczone koordynaty znajduja sie na ujeciu.
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+			# Wyciagniecie ROI, czyli interesujacego nas obszaru i zamienienie go
+			# z BGR na RGB
+			# Dodatkowo zmieniamy rozmiary wykrytej twarzy na 224x224
+			twarz = klatka[startY:endY, startX:endX]
+			twarz = cv2.cvtColor(twarz, cv2.COLOR_BGR2RGB)
+			twarz = cv2.resize(twarz, (224, 224))
+			twarz = img_to_array(twarz)
+			twarz = preprocess_input(twarz)
 
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
+			# Dodanie twarzy i jej koordynatow do odpowiednich tablic
+			twarze.append(twarz)
 			locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
-	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
-		faces = np.array(faces, dtype="float32")
-		preds = maskNet.predict(faces, batch_size=32)
+	# Do kolejnego etapu przechodzimy tylko, jezeli wykrylismy min 1 twarz
+	if len(twarze) > 0:
+		# Na wykrytych twarzach przeprowadzamy kolejne oszacowanie, tym razem
+		# sprawdzamy, z jakim prawdopodobienstwem na twarzy znajduje sie maska
+		twarze = np.array(twarze, dtype="float32")
+		prawdopodobienstwo_maski = maskNet.predict(twarze, batch_size=32)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
-	return (locs, preds)
+	# zwracamy krotke (twarz, lokacja)
+	return (locs, prawdopodobienstwo_maski)
 
-# construct the argument parser and parse the arguments
+# Przygotowujemy zmienne i podajemy lokalizacje modeli do wykrywania twarzy i masek
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", type=str,
-	default="face_detector",
-	help="path to face detector model directory")
+ap.add_argument("-f", "--twarz", type=str,
+	default="wykrywacz_twarzy",
+	help="sciezka do folderu z modelem do wykrywania twarzy")
 ap.add_argument("-m", "--model", type=str,
 	default="mask_detector.model",
-	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
+	help="sciezka do folderu z modelem do wykrywania twarzy")
+ap.add_argument("-c", "--prawdopodobienstwo", type=float, default=0.5,
+	help="minimalne prawdopodobienstwo do filtrowania twarzy")
 args = vars(ap.parse_args())
 
-# load our serialized face detector model from disk
-print("[INFO] loading face detector model...")
-prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
+# Wczytanie modelu do wykrywania twarzy z dysku
+print("[INFO] ladowanie modelu do wykrywania twarzy...")
+prototxtPath = os.path.sep.join([args["twarz"], "deploy.prototxt"])
+weightsPath = os.path.sep.join([args["twarz"],
 	"res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
-print("[INFO] loading face mask detector model...")
+# Wczytanie modelu do wykrywania maseczek z dysku
+print("[INFO] ladowanie modelu do wykrywania maseczek...")
 maskNet = load_model(args["model"])
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
+# inicjalizowanie strumienia z kamery
+print("[INFO] startowanie przesylania obrazu...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-# loop over the frames from the video stream
+# iterowanie po klatkach z strumienia z kamery
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	# Pobranie klatki obrazu i zmiana jego rozmiaru
+	klatka = vs.read()
+	klatka = imutils.resize(klatka, width=400)
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+	# Wykryj twarze na ujeciu i oszacuj czy sa w maseczc
+	(locs, prawdopodobienstwo_maski) = wykryj_i_oszacuj_maske(klatka, faceNet, maskNet)
 
-	# loop over the detected face locations and their corresponding
-	# locations
-	for (box, pred) in zip(locs, preds):
+	# Petla iterujaca po wszystkich wykrytych lokacjach twarzy
+	for (box, pred) in zip(locs, prawdopodobienstwo_maski):
 		# unpack the bounding box and predictions
 		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
+		(maska, bezMaski) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-		if label == "No Mask":
-			notification.notify(
-				title = "***No Mask Detected***",
-                		message = "Wear Mask to stay safe! ",
-                		app_icon = "images/1.ico",    #ico file should be downloaded
-                		timeout = 1
-            		)
+		# Ustalenie etykiety i koloru obramowania wykrytych obiektow na obrazie
+		label = "Maska" if maska > bezMaski else "Bez Maski"
+		color = (0, 255, 0) if label == "Maska" else (0, 0, 255)
 			
-			
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+		# Dodanie prawdopodobienstwa do etykiety obiektu
+		label = "{}: {:.2f}%".format(label, max(maska, bezMaski) * 100)
 
-		# display the label and bounding box rectangle on the output
-		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
+		# Wystwietlenie etykiety i obramowania na kadrze
+		cv2.putText(klatka, label, (startX, startY - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		cv2.rectangle(klatka, (startX, startY), (endX, endY), color, 2)
 
-		# Alarm when "No Mask" detected
-		if mask < withoutMask:
+		# Tutaj reagujemy na makse lub jej brak
+		if maska < bezMaski:
 			path = os.path.abspath("Alarm.wav")
 			playsound(path)
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
+	# wyswietlenie klatki
+	cv2.imshow("klatka", klatka)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
+	# Wylaczenie programu przyciskiem q
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
+# "Sprzatanie", wylaczenie strumienia z kamery i zniszczenie wszystkich okien
 cv2.destroyAllWindows()
 vs.stop()
 
